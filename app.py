6ca473454cbded35e6b0ac86b9766cdc5ffd5415
vabhishek-me <abhishek.k.v76076@gmail.com>
2018-07-30 21:46:14 +0530

    404 error page

--- app.py
+++ app.py
@@ -6,9 +6,13 @@ app.config['FLASKS3_BUCKET_NAME'] = 's3-bucket-for-flask'
 s3 = FlaskS3(app)
 
 @app.route("/")
-def hello():
+def home():
     data = {
         "title": "Flask WebApp",
         "body": "Flask Serverless WebApp"
     }
     return render_template('index.html', data=data)
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
