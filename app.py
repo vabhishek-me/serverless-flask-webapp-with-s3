e1d1bdceab0132314231c64ea597cf74a41ad347
vabhishek-me <abhishek.k.v76076@gmail.com>
2018-07-30 20:51:55 +0530

    added templates - jinja2

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    data = {
+        "title": "Flask WebApp",
+        "body": "Flask WebApp"
+    }
+    return render_template('index.html', data=data)
