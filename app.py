6ccddd37f5adbffaa9b5cbf959bbac8f53e3341b
vabhishek-me <abhishek.k.v76076@gmail.com>
2018-07-30 21:53:11 +0530

    500 page

--- app.py
+++ app.py
@@ -13,6 +13,10 @@ def home():
     }
     return render_template('index.html', data=data)
 
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template('500.html'), 500
+
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('404.html'), 404
